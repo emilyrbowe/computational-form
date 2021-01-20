50a9515d0e77ffcfe5ff3a20a868a8a53e1b4ccc
Emily Bowe <emilyrbowe@gmail.com>
2021-01-20 14:23:06 -0500

    Adding Comp Form files

---
+++ 12_generated_text/20201207_TextGen2_coverpage/sketch.js
@@ -0,0 +1,76 @@
+function preload() {
+  main();
+}
+
+function setup() {
+
+  // layout determiner
+  var layoutPos = floor(random(10));
+  // console.log(layoutPos);
+}
+
+var tracery;
+
+// require tracery.min.js
+
+// const storyGrammar = {
+//   'top': ['#div#'],
+//   'div': [
+//     '<div style="border: #borderSize# #borderType# #borderColor#;  color: #borderColor#; text-align: center; text-transform: uppercase; font-family: sans-serif; margin: 10px;">#content#</div>'
+//   ],
+//   'content': ['#div#', '#div# #div#', '#word#', '#word#'],
+//   'borderSize': ['1px', '4px', '10px'],
+//   'borderType': ['solid', 'dashed', 'dotted'],
+//   'borderColor': ['red', 'green', 'blue'],
+//   'word': ['<span style="font-size: 30px">#adj# #animal##char#</span>'],
+//   'adj': ['angry', 'happy', 'major', 'minor', 'speedy', 'wrathful'],
+//   'animal': ['cat', 'mouse'],
+//   'char': ['!', '?']
+// }
+//
+//
+// function main() {
+//   let grammar = tracery.createGrammar(storyGrammar);
+//   let story = grammar.flatten('#top#');
+//
+//   const storyDiv = document.createElement('div');
+//   storyDiv.innerHTML = story;
+//
+//   document.body.insertAdjacentElement('beforeend', storyDiv);
+// }
+
+// require /text/tracery.min.js
+
+const storyGrammar = {
+  'top': ['#div#'],
+  'div': [
+    '<div style="border: #borderSize# #borderType# #borderColor#;  color: #borderColor#; text-align: center; text-transform: uppercase; font-family: sans-serif; margin: 10px;">#content#</div>'
+  ],
+  'content': ['#div#', '#div# #div#', '#word#', '#word#'],
+  'borderSize': ['1px', '4px', '10px'],
+  'borderType': ['solid', 'dashed', 'dotted'],
+  'borderColor': ['red', 'green', 'blue'],
+  'word': ['<span style="font-size: 30px">#adj# #animal##char#</span>'],
+  'adj': ['angry', 'happy', 'major', 'minor', 'speedy', 'wrathful'],
+  'animal': ['cat', 'mouse'],
+  'char': ['!', '?']
+};
+
+
+function main() {
+  let grammar = tracery.createGrammar(storyGrammar);
+  let story = grammar.flatten('#top#');
+
+  const storyDiv = document.getElementsByName('coverpage');
+  storyDiv.createElement('div');
+  storyDiv.innerHTML = story;
+
+  document.body.insertAdjacentElement('beforeend', storyDiv);
+}
+
+
+setTimeout(main, 10);
+
+
+
+setTimeout(main, 10);
